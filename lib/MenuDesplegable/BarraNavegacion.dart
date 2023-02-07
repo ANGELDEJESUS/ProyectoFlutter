fe11e6c10f8d877bda31e68599f9faf99fb9b530
angel_anguiano_camacho <anguianolab2@gmail.com>
2023-02-07 17:51:14 +0000

    se agrego funcionalidad de bottom bar de navegacion

--- lib/MenuDesplegable/BarraNavegacion.dart
+++ lib/MenuDesplegable/BarraNavegacion.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:helloworld/MenuDesplegable/BarraMenu.dart';
+import 'package:helloworld/MenuDesplegable/MenuDesplegable.dart';
+import 'package:helloworld/Pantallas/MyHomePage.dart';
 import 'package:helloworld/Pantallas/Perfil.dart';
 
 class BarraNavegacion extends StatefulWidget {
@@ -8,51 +13,73 @@ class BarraNavegacion extends StatefulWidget {
 
 class _BarraNavegacion extends State<BarraNavegacion> {
   int _currentIndex = 0;
+  String encabezado = "Home";
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+      if (_currentIndex == 0) {
+        encabezado = "Home";
+      }
+      if (_currentIndex == 1) {
+        encabezado = "Musica";
+      }
+      if (_currentIndex == 2) {
+        encabezado = "Maps";
+      }
+    });
+  }
+
+  List<Widget> _pages = <Widget>[
+    Icon(
+      Icons.home,
+      size: 150,
+    ),
+    Icon(
+      Icons.camera,
+      size: 150,
+    ),
+    Icon(
+      Icons.chat,
+      size: 150,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.shifting,
-      backgroundColor: Color(0xff232225),
-      selectedItemColor: Colors.amber,
-      unselectedItemColor: Colors.amber.withOpacity(0.6),
-      selectedFontSize: 14,
-      unselectedFontSize: 14,
-      onTap: (value) {
-        // Respond to item press.
-        setState(() => _currentIndex = value);
-        print(_currentIndex);
-        if (_currentIndex == 1) {
-          setState(() {
-            Navigator.pushNamed(context, '/musica');
-          });
-        }
-        if (_currentIndex == 0) {
-          setState(() {
-            Navigator.pushNamed(context, '/');
-          });
-        }
-        if (_currentIndex == 2) {
-          setState(() {
-            Navigator.pushNamed(context, '/musica');
-          });
-        }
-      },
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: "Home",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.music_note),
-          label: "Videos",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.location_on),
-          label: "Noticias",
-        ),
-      ],
+    return Scaffold(
+      drawer: MenuDesplegable(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        backgroundColor: Color(0xff232225),
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.amber.withOpacity(0.6),
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.music_note),
+            label: "Videos",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: "Noticias",
+          ),
+        ],
+        currentIndex: _currentIndex,
+      ),
+      body: Center(
+        child: Column(children: [
+          BarraMenu(encabezado),
+          _pages.elementAt(_currentIndex),
+        ]),
+      ),
     );
   }
 }
